a8c05e8724334af0c2a117a698c077795c85009a
pedestrianlove <37164678+pedestrianlove@users.noreply.github.com>
2024-06-28 23:09:56 +0800

    Revert "Revert "在系所的下拉式選單裡加入optgroup以學院分類系所""

--- crawler/crawler/pipelines.py
+++ crawler/crawler/pipelines.py
@@ -21,20 +21,27 @@ class CrawlerPipeline:
         for course_id in self.data_obj:
             department_id = self.data_obj[course_id]['department_id']
             department_name = self.data_obj[course_id]['department']
-            department[department_id] = department_name
-        department_file = '../course-data/' + str(semester['year']) + str(semester['semester']) + '-dep-data.json'
+            college_name = self.data_obj[course_id]['college']
+            department[department_id] = {
+                'name': department_name, 'college': college_name}
+        department_file = '../course-data/' + \
+            str(semester['year']) + \
+            str(semester['semester']) + '-dep-data.json'
         with open(department_file, 'w') as outfile:
-            json.dump(department, outfile, ensure_ascii=False, indent=None, separators=(',', ':'))
+            json.dump(department, outfile, ensure_ascii=False,
+                      indent=None, separators=(',', ':'))
 
         # Save the course data
-        course_file = '../course-data/' + str(semester['year'])+str(semester['semester']) + '-data.json'
+        course_file = '../course-data/' + \
+            str(semester['year'])+str(semester['semester']) + '-data.json'
         processed_course = {}
         for course_id in self.data_obj:
             self.data_obj[course_id].pop('department')
+            self.data_obj[course_id].pop('college')
             processed_course[course_id] = self.data_obj[course_id]
         with open(course_file, 'w') as outfile:
-            json.dump(self.data_obj, outfile, ensure_ascii=False, indent=None, separators=(',', ':'))
-
+            json.dump(self.data_obj, outfile, ensure_ascii=False,
+                      indent=None, separators=(',', ':'))
 
     def process_item(self, item, spider):
         self.data_obj[item["id"]] = item
